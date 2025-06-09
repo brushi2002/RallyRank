3a1c0e4f79fc387b528c1124279859f9a11dc202
brushi2002 <roscoe@ericsb.com>
2025-06-09 16:32:37 -0700

    "icon stuff"

---
+++ app/_layout copy.tsx
@@ -0,0 +1,25 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack, Slot } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import 'react-native-gesture-handler';
+import { GlobalProvider } from '@/lib/global-provider';
+import './global.css';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+//SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  return (
+    <GlobalProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(root)" />
+      </Stack>
+    </GlobalProvider>
+  );
+}
