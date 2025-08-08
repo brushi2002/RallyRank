9bac87490b71ac031224bfe91bdc10e266afdb74
brushi2002 <roscoe@ericsb.com>
2025-08-08 16:36:42 -0700

    more config stuff

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,23 @@ module.exports = function (api) {
             jsxImportSource: 'nativewind',
           },
         ],
-      ]
+      ],
+      plugins: [
+        [
+          'module:react-native-dotenv',
+          {
+            envName: 'APP_ENV',
+            moduleName: '@env',
+            path: '.env',
+            blocklist: null,
+            allowlist: null,
+            blacklist: null, // DEPRECATED
+            whitelist: null, // DEPRECATED
+            safe: false,
+            allowUndefined: true,
+            verbose: false,
+          },
+        ],
+      ],
     };
   };
---
+++ types/env.d.ts
@@ -0,0 +1,11 @@
+declare module '@env' {
+  export const EXPO_PUBLIC_APPWRITE_ENDPOINT: string;
+  export const EXPO_PUBLIC_APPWRITE_PROJECT_ID: string;
+  export const EXPO_PUBLIC_APPWRITE_DATABASE_ID: string;
+  export const EXPO_PUBLIC_APPWRITE_PLAYER_COLLECTION_ID: string;
+  export const EXPO_PUBLIC_APPWRITE_MEMBER_COLLECTION_ID: string;
+  export const EXPO_PUBLIC_APPWRITE_MATCH_COLLECTION_ID: string;
+  export const EXPO_PUBLIC_APPWRITE_LEAGUE_COLLECTION_ID: string;
+  export const EXPO_PUBLIC_APPWRITE_GLOBAL_LEAGUE_ID: string;
+  export const EXPO_PUBLIC_APPWRITE_STORAGE_ID: string;
+}
