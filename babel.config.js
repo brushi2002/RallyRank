2dd5d5e5e7c9d5d4c6a632c87286f58ba472d262
brushi2002 <roscoe@ericsb.com>
2025-08-09 10:05:06 -0700

    again

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,32 @@ module.exports = function (api) {
             jsxImportSource: 'nativewind',
           },
         ],
-      ]
+      ],
+      plugins: [
+        [
+          'module-resolver',
+          {
+            root: ['./'],
+            alias: {
+              '@': './',
+            },
+          },
+        ],
+        [
+          'module:react-native-dotenv',
+          {
+            envName: 'APP_ENV',
+            moduleName: '@env',
+            path: '.env',
+            blocklist: null,
+            allowlist: null,
+            blacklist: null, // DEPRECATED
+            whitelist: null, // DEPRECATED
+            safe: false,
+            allowUndefined: true,
+            verbose: false,
+          },
+        ],
+      ],
     };
   };
