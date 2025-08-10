766ccb18541194af6193ab6ba58cabfb4c583beb
brushi2002 <roscoe@ericsb.com>
2025-08-09 17:44:56 -0700

    again

--- components/__tests__/HelloWave.test 3.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
-import { HelloWave } from '../HelloWave';
-console.log('HelloWave:', HelloWave);
-
-test('renders correctly', () => {
-  const { getByText } = render(<HelloWave />);
-  expect(getByText('Hello, Wave!')).toBeTruthy();
-});
