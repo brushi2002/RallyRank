3a14a39642957e8b856ded1866469a164d396f98
brushi2002 <roscoe@ericsb.com>
2025-08-09 18:33:09 -0700

    again

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,19 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+    presets: ['babel-preset-expo'],
+    plugins: [
       'nativewind/babel',
-  ],
-  plugins: [
-    'react-native-reanimated/plugin',
-  ],
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './',
+          },
+        },
+      ],
+    ],
   };
 };
