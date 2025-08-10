81e946632a4cfde0ce4da1173aa5030fb1fc0d1d
brushi2002 <roscoe@ericsb.com>
2025-08-09 19:05:09 -0700

    again

--- babel.config.js
+++ babel.config.js
@@ -5,15 +5,6 @@ module.exports = function (api) {
     plugins: [
       'nativewind/babel',
       'react-native-reanimated/plugin',
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './',
-          },
-        },
-      ],
     ],
   };
 };
